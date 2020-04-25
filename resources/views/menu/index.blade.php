2abc2806b536b145ad6d79b79ed229a190d8e264
Dave Guillaume <me@daveg.com>
2020-04-25 18:13:16 -0400

    Made layouts responsive

---
+++ resources/views/menu/index.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+
+@section('content')
+<div class="content-box">
+  <h1>Hello</h1>
+</div>
+@endsection
---
+++ resources/views/pages/giftcards.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+
+@section('content')
+<div class="">
+  <h1>Giftcards page</h1>
+</div>
+@endsection
